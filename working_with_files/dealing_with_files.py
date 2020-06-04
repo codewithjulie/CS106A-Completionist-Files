48e35a39f797d100d737aa884ad2da4027b8ef76
the-unXplainable <bluestrawberries33@gmail.com>
2020-06-04 15:31:48 -0700

    finished files file

--- working_with_files/dealing_with_files.py
+++ working_with_files/dealing_with_files.py
@@ -4,18 +4,22 @@
 file = open('filename.txt')
     
 for line in file:
-    print(line)           # Prints each line including whitespace
-    print(line.strip())   # This removes whitespace and \n
+    print(line)             # Prints each line including whitespace
+    print(line.strip())     # This removes whitespace and \n
+
+file.close()                # Was not discussed in class, but can do this to close
 
 
 #How to open a file and automatically close it (the proper way)
 with open('filename.txt') as file:  # Notice the colon, remaining code must be indented
     for line in file:
         print(line)
-    print(file.readline())      # .readline returns a string and only one line
-    print(file.readline(10))    # takes in an argument, prints 10 characters of line
-    print(file.readlines())     # .readlines() returns a list of all lines
-    print(list(file))           # same as file.readlines()
+    print(file.read())            # Returns a string of the whole file
+    print(file.read(50))          # Returns a string of 50 characters from the file
+    print(file.readline())        # readline returns a string and only one line
+    print(file.readline(10))      # takes in an argument, prints 10 characters of line
+    print(file.readlines())       # readlines() returns a list of all lines
+    print(list(file))             # same as file.readlines()
 
 
 # Can take in another argument 'mode':
@@ -28,11 +32,20 @@ with open('states_us.csv', 'r') as f: # r for read, w for write, a for append
 
 
 # How to open and write in a file:
-with open('filename.txt', 'w') as file:           # w for write
-    file.write("I am writing in a new line\n")    # Overwrites this to our file
+with open('filename.txt', 'w') as file:      # 'w' for write, if file exists it will reinitialize as empty
+    file.write("I am writing a new line\n")  # You will lose any previous contents
 
 
 # How to open and write in a file without overwriting:
 with open('filename.txt', 'a') as file:           # a for append
-    file.write("I am not overwriting our file")   # appends to the end of the file
+    file.write("I am adding to the end of a file")   # appends to the end of the file
+
+
+# How to read data and write into a different file
+infile = open('reading.txt', 'r')
+outfile = open('writing.txt', 'w')
+
+for line in infile:
+    outfile.write(line)
+    
 
