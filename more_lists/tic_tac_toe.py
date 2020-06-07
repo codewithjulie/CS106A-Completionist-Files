df4e5039122042aeb1b80ae424ba0393e964b2d6
the-unXplainable <bluestrawberries33@gmail.com>
2020-06-06 17:56:44 -0700

    testing

--- more_lists/tic_tac_toe.py
+++ more_lists/tic_tac_toe.py
@@ -12,12 +12,10 @@ Milestones:
 . 
 """
 
-
 import os
 clear = lambda: os.system('clear')
 
-
-
+# Create board (3 x 3 grid)
 def game_board(length=3):
     grid = []
     for i in range(length):
@@ -28,15 +26,22 @@ def game_board(length=3):
     return grid
 
 
+# Print the board to terminal
 def display_board(grid):
     for row in grid:
         print(row)
 
 
-def update_position(grid, row, col, player):
-    grid[row][col] = player
+# Determine next player
+def change_players(player):
+    if player == "O":
+        player = "X"
+    else:
+        player = "O"
+    return player
 
 
+# Get position from player
 def get_player_move(board, player):
     valid = False
     while not valid:
@@ -48,6 +53,8 @@ def get_player_move(board, player):
             break
     return row, col
 
+
+# Check if position is within the grid and not marked
 def valid_move(board, row, col):
     if row < 0 or row > 2 or col < 0 or col > 2:
         return False
@@ -56,6 +63,21 @@ def valid_move(board, row, col):
     return True
 
 
+# Mark the board
+def update_position(grid, row, col, player):
+    grid[row][col] = player
+
+
+# Determine if the game is over
+def game_over(board):
+    return three_in_row_horizontal(board) or \
+        three_in_row_vertical(board) or \
+            three_in_row_diagonal_up(board) or \
+                three_in_row_diagonal_down(board) or \
+                    tie(board)
+
+
+# Helper method to determine if there is a tie
 def tie(grid):
     for row in grid:
         for col in row:
@@ -64,15 +86,21 @@ def tie(grid):
     return True
 
 
-def game_over(board):
-    return three_in_row_horizontal(board) or three_in_row_vertical(board) or three_in_row_diagonal_up(board) or three_in_row_diagonal_down(board)
-
+# Helper method to determine if a player wins horizontally
 def three_in_row_horizontal(grid):
     for row in grid:
         if len(set(row)) == 1 and "_" not in row:
             return True
     return False
 
+
+# Helper method to determine if a player wins vertically
+def three_in_row_vertical(grid):
+    flipped_grid = flip_grid(grid)
+    return three_in_row_horizontal(flipped_grid)
+
+
+# Helper method that transposes grid used to check vertical win
 def flip_grid(grid):
     flipped_grid = []
     for i in range(len(grid)):
@@ -82,11 +110,8 @@ def flip_grid(grid):
         flipped_grid.append(new_row)
     return flipped_grid
 
-def three_in_row_vertical(grid):
-    flipped_grid = flip_grid(grid)
-    return three_in_row_horizontal(flipped_grid)
 
-
+# Helper method to determine if a player wins diagonally from top left
 def three_in_row_diagonal_down(grid):
     lst = []
     for row in range(len(grid)):
@@ -96,6 +121,7 @@ def three_in_row_diagonal_down(grid):
     return False
 
 
+# Helper method to determine if a player wins diagonally from bottom left
 def three_in_row_diagonal_up(grid):
     lst = []
     lst.append(grid[0][2])
@@ -104,30 +130,23 @@ def three_in_row_diagonal_up(grid):
     if len(set(lst)) == 1 and lst[0] != "_":
         return True
     return False
-            
-
-def change_players(player):
-    if player == "O":
-        player = "X"
-    else:
-        player = "O"
-    return player
 
 def main():
-    clear()
+    clear()     # Start with a empty terminal
     board = game_board()
     display_board(board)
-    player = "O"
-    while not tie(board) and not game_over(board):
-        player = change_players(player)
+    player = "X"
+    while not game_over(board):
         row, col = get_player_move(board, player)
         update_position(board, row, col, player)
         clear()
         display_board(board)
-    if tie(board):
-        print("It's a tie!")
-    if game_over(board):
-        print(f"Player {player} wins!")
+        if tie(board):
+            print("It's a tie!")
+        if game_over(board):
+            print(f"Player {player} wins!")
+        player = change_players(player)
 
 if __name__ == "__main__":
     main()
+    
